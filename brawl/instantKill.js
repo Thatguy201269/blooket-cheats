576fc1e5ec0019113ade8238aee0346e89fe8ecb
SuperHackz <79522273+SuperHackz@users.noreply.github.com>
2025-02-17 08:22:37 +0000

    keep it shhhh

--- brawl/instantKill.js
+++ brawl/instantKill.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * Source: https://github.com/SuperHackz/blooket-cheats 994@gmail.com
+ * Source: https://github.com/SuperHackz/blooket-cheats 
 */
 
 /* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
